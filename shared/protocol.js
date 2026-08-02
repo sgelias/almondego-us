8c596f9493d3f38e7bb6433a2719eb522d0dfe8f
Samuel Galvão Elias <sgelias@outlook.com>
2026-08-01 23:31:38 -0300

    feat(net): extend protocol with role/task/kill/meeting/vote message types

--- shared/protocol.js
+++ shared/protocol.js
@@ -6,6 +6,19 @@ export const MESSAGE_TYPE = {
   STATE: 'state',
   START: 'start',
   ERROR: 'error',
+
+  ROLE: 'role',
+  TELEPORT: 'teleport',
+  TASK_COMPLETE: 'taskComplete',
+  KILL: 'kill',
+  CALL_MEETING: 'callMeeting',
+  VOTE: 'vote',
+  VENT: 'vent',
+  TASKS_PROGRESS: 'tasksProgress',
+  PLAYER_DIED: 'playerDied',
+  MEETING_STARTED: 'meetingStarted',
+  MEETING_RESULT: 'meetingResult',
+  GAME_OVER: 'gameOver',
 }
 
 const KNOWN_TYPES = new Set(Object.values(MESSAGE_TYPE))
